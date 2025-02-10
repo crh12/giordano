4666523d53a5f7534877debb37ac052cd5fefad9
crh12 <scrin012@gmail.com>
2025-02-10 15:08:20 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,63 @@
+const mainSlide = document.querySelectorAll('.main_bnr .swiper-slide');
+const mainSlideTotal = document.querySelector('.main_bnr .slide_total');
+const mainSlideCurrent = document.querySelector('.main_bnr .slide_current');
+const gSlideCurrent = document.querySelector('.gmotion_container .slide_current');
+
+mainSlideTotal.textContent = mainSlide.length;
+
+const mainSwiper = new Swiper('.main_swiper',{
+  // autoplay:{delay:2500,},
+  loop:true,
+  navigation:{
+    nextEl:'.main_bnr .next',
+    prevEl:'.main_bnr .prev',
+  },
+  on:{
+    slideChange:function(){
+      mainSlideCurrent.textContent = this.realIndex+1;
+    }
+  },
+});
+
+const bestSwiper = new Swiper('.best_swiper1',{
+  loop:true,
+  navigation:{
+    nextEl:'.best_slide_btn .next',
+    prevEl:'.best_slide_btn .prev',
+  },
+  slidesPerView:5,
+  spaceBetween:25,
+});
+
+const newSwiper = new Swiper('.new_swiper1',{
+  loop:true,
+  navigation:{
+    nextEl:'.new_slide_btn .next',
+    prevEl:'.new_slide_btn .prev',
+  },
+  slidesPerView:5,
+  spaceBetween:25,
+});
+
+const mdSwiper = new Swiper('.md_swiper',{
+  loop:true,
+  navigation:{
+    nextEl:'.md_slide_btn .next',
+    prevEl:'.md_slide_btn .prev',
+  }
+});
+
+const gSwiper = new Swiper('.g_swiper',{
+  loop:true,
+  navigation:{
+    nextEl:'.gmotion_container .next',
+    prevEl:'.gmotion_container .prev',
+  },
+  slidesPerView:4,
+  slidesPerGroup:4,
+  spaceBetween:(160 / 3),
+  pagination:{
+    el:'.gmotion_container .slide_current',
+    type:'fraction',
+  }
+});
